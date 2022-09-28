6f8f8e2c70a23da39247d5518e72f5dd72b84718
Jonathan Elías Sor Monroy <jsor1@cofal.net>
2022-09-27 19:10:58 -0600

    ..

--- lib/page/Aerolinea/AgregarAerolinea.dart
+++ lib/page/Aerolinea/AgregarAerolinea.dart
@@ -19,6 +19,7 @@ class _AgregarAerolinea extends State<AgregarAerolinea> {
   final txtTelefono = TextEditingController();
   final txtCodigo = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -30,64 +31,176 @@ class _AgregarAerolinea extends State<AgregarAerolinea> {
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    TextFormField(
-                      controller: txtNombre,
-                      keyboardType: TextInputType.name,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        nombreAerolinea = value;
-                      },
-                      decoration: const InputDecoration(
-                          labelText: 'Nombre de la Aerolínea'),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 3,
-                          child: TextField(
-                            controller: txtCodigo,
-                            maxLength: 3,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            onChanged: (value) {
-                              codigo = value;
-                            },
-                            decoration:
-                                const InputDecoration(labelText: 'Código'),
-                          ),
-                        ),
-                        const Expanded(
-                          flex: 1,
-                          child: Text(''),
-                        ),
-                        Expanded(
-                          flex: 6,
-                          child: TextField(
-                            controller: txtTelefono,
-                            maxLength: 8,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            onChanged: (value) {
-                              telefonoAerolinea = value;
-                            },
-                            decoration:
-                                const InputDecoration(labelText: 'Teléfono'),
-                          ),
-                        )
-                      ],
-                    ),
-                    TextField(
-                      controller: txtCorreo,
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        correoAerolinea = value;
-                      },
-                      decoration: const InputDecoration(labelText: 'Correo'),
-                    ),
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Por favor ingrese información';
+                                }
+                              },
+                              controller: txtNombre,
+                              keyboardType: TextInputType.name,
+                              textAlign: TextAlign.center,
+                              onChanged: (value) {
+                                nombreAerolinea = value;
+                              },
+                              decoration: const InputDecoration(
+                                  labelText: 'Nombre de la Aerolínea'),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 3,
+                                  child: TextFormField(
+                                    controller: txtCodigo,
+                                    maxLength: 3,
+                                    keyboardType: TextInputType.number,
+                                    textAlign: TextAlign.center,
+                                    onChanged: (value) {
+                                      codigo = value;
+                                    },
+                                    decoration: const InputDecoration(
+                                        labelText: 'Código'),
+                                  ),
+                                ),
+                                const Expanded(
+                                  flex: 1,
+                                  child: Text(''),
+                                ),
+                                Expanded(
+                                  flex: 6,
+                                  child: TextField(
+                                    controller: txtTelefono,
+                                    maxLength: 8,
+                                    keyboardType: TextInputType.number,
+                                    textAlign: TextAlign.center,
+                                    onChanged: (value) {
+                                      telefonoAerolinea = value;
+                                    },
+                                    decoration: const InputDecoration(
+                                        labelText: 'Teléfono'),
+                                  ),
+                                )
+                              ],
+                            ),
+                            TextFormField(
+                              controller: txtCorreo,
+                              keyboardType: TextInputType.emailAddress,
+                              textAlign: TextAlign.center,
+                              onChanged: (value) {
+                                correoAerolinea = value;
+                              },
+                              decoration:
+                                  const InputDecoration(labelText: 'Correo'),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                    flex: 2,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.blue,
+                                          onPrimary: Colors.white),
+                                      child: const Text('Guardar'),
+                                      onPressed: () {
+                                        if (_formKey.currentState!.validate()) {
+                                          // If the form is valid, display a snackbar. In the real world,
+                                          // you'd often call a server or save the information in a database.
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                content:
+                                                    Text('Processing Data')),
+                                          );
+                                        }
+                                        bool correo = false;
+                                        if (correoAerolinea != null &&
+                                            nombreAerolinea != null &&
+                                            telefonoAerolinea != null &&
+                                            telefonoAerolinea != '' &&
+                                            nombreAerolinea != '' &&
+                                            telefonoAerolinea != '') {
+                                          correo = RegExp(
+                                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                              .hasMatch(correoAerolinea!);
+                                          if (correo == false) {
+                                            AlertaUnBoton(
+                                                context,
+                                                'Error',
+                                                'Por favor ingrese un correo válido',
+                                                'Aceptar',
+                                                pAccion: () =>
+                                                    Navigator.pop(context));
+                                          } else if (telefonoAerolinea
+                                                      ?.length !=
+                                                  8 ||
+                                              codigo?.length != 3) {
+                                            AlertaUnBoton(
+                                                context,
+                                                'Error',
+                                                'Por favor ingrese un número telefonico válido',
+                                                'Aceptar',
+                                                pAccion: () =>
+                                                    Navigator.pop(context));
+                                          } else {
+                                            //Todas las validaciones fueron correctas y se guarda la información
+                                            try {
+                                              var respuesta =
+                                                  Aerolinea.addAerolinea(
+                                                      nombre: nombreAerolinea
+                                                          .toString(),
+                                                      codigo: codigo.toString(),
+                                                      telefono:
+                                                          telefonoAerolinea
+                                                              .toString(),
+                                                      correo: correoAerolinea
+                                                          .toString());
+                                              notificacion(
+                                                  context,
+                                                  'Registro',
+                                                  'Se ha registrado correctamente',
+                                                  0);
+                                              limpiarCampos();
+                                            } catch (e) {
+                                              AlertaUnBoton(context, 'Error',
+                                                  e.toString(), 'Aceptar',
+                                                  pAccion: () =>
+                                                      Navigator.pop(context));
+                                            }
+                                          }
+                                        } else {
+                                          AlertaUnBoton(
+                                              context,
+                                              'Error',
+                                              'Por favor llene todos los campos',
+                                              'Aceptar',
+                                              pAccion: () =>
+                                                  Navigator.pop(context));
+                                        }
+                                      },
+                                    )),
+                                const Expanded(
+                                  flex: 1,
+                                  child: Text(''),
+                                ),
+                                Expanded(
+                                    flex: 2,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.grey,
+                                          onPrimary: Colors.white),
+                                      child: const Text('Limpiar'),
+                                      onPressed: () {
+                                        limpiarCampos();
+                                      },
+                                    )),
+                              ],
+                            ),
+                          ],
+                        )),
                     Row(
                       children: <Widget>[
                         Expanded(
